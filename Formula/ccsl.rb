c9ad0e42bd5d9ffea85e58b90dc5558e971bd464
usedhonda <usedhonda@gmail.com>
2026-06-11 11:35:41 +0100

    ccsl 1.0.17

--- Formula/ccsl.rb
+++ Formula/ccsl.rb
@@ -3,8 +3,8 @@ class Ccsl < Formula
 
   desc "Enhanced status line for Claude Code"
   homepage "https://github.com/usedhonda/statusline"
-  url "https://files.pythonhosted.org/packages/d1/20/b113ec74b1584db5ac006e6f03a3511cb1c1ef3555f8ecb640b1b6b2c366/ccsl-1.0.16.tar.gz"
-  sha256 "835bda6fd90bb4cc5c9854141f096906ae4676bb2e13099aef660242a986a3ec"
+  url "https://files.pythonhosted.org/packages/c3/a3/faf56e459465d49ef56acbeae18fb10b387a056785fbc4b2bd6befda85ee/ccsl-1.0.17.tar.gz"
+  sha256 "e28166a4cd73179b7d9fea5530bb1e35e2a97b6b04ab2514f6a581b64637ab5a"
   license "MIT"
 
   depends_on "python@3.12"
